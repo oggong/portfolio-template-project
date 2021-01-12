64ce6fbb9c68762582a3e181987d38f5e50dd2fe
oggong <49095304+oggong@users.noreply.github.com>
2021-01-12 18:56:43 +0900

    푸터 변경

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -21,8 +21,8 @@
         <div class="container">
             <div class="row row-grid align-items-center my-md">
                 <div class="col-lg-6">
-                    <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
-                    <h4 class="mb-0 font-weight-light">Let's get in touch on any of these platforms.</h4>
+                    <h3 class="text-primary font-weight-light mb-2">Thank you for visiting site!</h3>
+                    <h4 class="mb-0 font-weight-light">THIS IS JINK STORY SITE</h4>
                 </div>
                 <div class="col-lg-6 text-lg-center btn-wrapper">
                     <a target="_blank" rel="noopener" href="https://twitter.com/creativetim"
@@ -52,13 +52,13 @@
                 <div class="col-md-6">
                     <div class="copyright">
                         &copy; {{year}}
-                        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Minseok Oh</a>
+                        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">JINK</a>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <ul class="nav nav-footer justify-content-end">
                         <li class="nav-item">
-                            <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Minseok Oh</a>
+                            <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">JINK</a>
                         </li>
                         <li class="nav-item">
                             <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank" rel="noopener">About
