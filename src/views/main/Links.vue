a947d9ab2caf49e85dfc5b7e57ad4c22c8a6d5b9
oggong <49095304+oggong@users.noreply.github.com>
2021-01-17 21:42:26 +0900

    0117 commit

--- src/views/main/Links.vue
+++ src/views/main/Links.vue
@@ -8,10 +8,33 @@
                     </h3>
                 </div>
                 <div class="col-sm-12 col-12">
-                    <tabs fill class="flex-column flex-md-row w-100 h-100">
-                        <tab-pane title="UI/UX Design">tt</tab-pane>
-                        <tab-pane title="Programming">tt</tab-pane>
-                        <tab-pane title="Graphic">ttt</tab-pane>
+                    <tabs fill class="flex-column flex-md-row w-100 h-100 mb-5">
+                       <card shadow slot-scope="{activeTabIndex}">
+                    <tab-pane key="tab1">
+                        <template slot="title">
+                            <i class="ni ni-collection mr-2"></i>First Project
+                        </template>
+
+                        <p class="description">첫 번째 프로젝트의 설명 입니다. 저는 이런 프로젝트를 만들었습니다. 짱이죠? </p>
+                       
+                    </tab-pane>
+
+                    <tab-pane key="tab2">
+                        <template slot="title">
+                            <i class="ni ni-collection mr-2"></i>Second Project
+                        </template>
+
+                        <p class="description">두 번째 프로젝트의 설명 입니다. 저는 이런 프로젝트를 만들었습니다. 짱이죠? </p>
+                    </tab-pane>
+
+                    <tab-pane key="tab3">
+                        <template slot="title">
+                            <i class="ni ni-collection mr-2"></i>Third Project
+                        </template>
+
+                        <p class="description">세 번째 프로젝트의 설명 입니다. 저는 이런 프로젝트를 만들었습니다. 짱이죠? </p>
+                    </tab-pane>
+                </card>
                     </tabs>
                 </div>
             </div>
@@ -20,7 +43,20 @@
 
 </template>
 <script>
-export default {};
+import Tabs from "@/components/Tabs/Tabs.vue";
+import TabPane from "@/components/Tabs/TabPane.vue";
+export default {
+    name: 'Links',
+    data(){
+        return {
+
+        }
+    },
+    components: {
+        Tabs,
+        TabPane
+    }
+};
 </script>
 <style>
 </style>
