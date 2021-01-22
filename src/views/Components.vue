04ba9dd5fe6a5a3ea2f0e12ff5155123c905f4ac
oggong <49095304+oggong@users.noreply.github.com>
2021-01-22 20:12:33 +0900

    0123 commit

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,20 +1,11 @@
 <template>
-    <div>
+    <div class="main">
       <Home></Home>
       <About></About>
       <Skills></Skills>
       <Education></Education>
       <Links></Links>
-        <!-- <hero></hero> -->
-        <!-- <basic-elements></basic-elements> -->
-        <!-- <inputs></inputs> -->
-        <!-- <custom-controls></custom-controls> -->
-        <!-- <navigation></navigation> -->
-        <!-- <javascript-components></javascript-components> -->
-        <!-- <icons></icons> -->
-        <!-- <examples></examples> -->
-        <!-- <download-section></download-section> -->
-        <!-- <carousel></carousel> -->
+      <Contact></Contact>
     </div>
 </template>
 <script>
@@ -24,37 +15,13 @@ import Skills from './main/Skills';
 import Education from './main/Education';
 import Links from './main/Links';
 
-
-
-import Hero from "./components/Hero";
-import BasicElements from "./components/BasicElements";
-import Inputs from "./components/Inputs";
-import CustomControls from "./components/CustomControls";
-import Navigation from "./components/Navigation";
-import JavascriptComponents from "./components/JavascriptComponents";
-import Carousel from "./components/Carousel";
-import Icons from "./components/Icons";
-import Examples from "./components/Examples";
-import DownloadSection from "./components/DownloadSection";
-
-
 export default {
   name: "components",
   components: {
     Home,
     About,
     Skills,
-    Links,
-    Hero,
-    BasicElements,
-    Inputs,
-    CustomControls,
-    Navigation,
-    JavascriptComponents,
-    Carousel,
-    Icons,
-    Examples,
-    DownloadSection,
+    Links, 
     Education
   }
 };
--- src/views/main/Contact.vue
+++ src/views/main/Contact.vue
@@ -4,8 +4,10 @@
             <div class="row">
                 <div class="col-sm-12 col-12">
                     <h3 class="h4 text-success font-weight-bold mb-4">
-                        CONTACT ME 
+                        Feel free to contact me for any work or suggestions below
                     </h3>
+
+                    <h5>bjk@gmail.com</h5>
                 </div>
             </div>
         </div>
