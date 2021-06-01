539dbb7982c83b9b45376579aff3058528d696f8
oggong <49095304+oggong@users.noreply.github.com>
2021-06-01 15:22:12 +0900

    App Header modify

--- src/views/main/About.vue
+++ src/views/main/About.vue
@@ -3,13 +3,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-3 col-6">
-                    <img v-lazy="'img/theme/about_image.png'" alt="Circle image"
-                        class="img-fluid rounded-circle shadow" style="width: 150px;">
+                    <img v-lazy="'img/theme/minseok_oh2.jpg'" alt="Circle image"
+                        class="img-fluid rounded-circle shadow" style="width: 200px; height: 200px;">
                 </div>
                 <div class="col-sm-9 col-6">
                     <h3>About Me</h3>
-                    <a href="#">JINKSTORY</a>
-                    <p>배프로 입니다. 뭐든지 잘 할수 있는 능력을 가지고 있습니다.</p>
+                    <a href="#">MINSEOK OH</a>
+                    <p>웹, 앱 프론트 엔드 개발자 오민석 입니다.</p>
                     <h3>Contact Details</h3>
                     <a href="#">JINK'S GITHUB</a>
                     <p>뭐든지 잘하는 배프로의 깃허브 입니다.</p>
